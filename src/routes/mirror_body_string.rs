83da3b2e97ecfdfd7060fa68ae5ea8bfc294dbbb
David López <davidlghellin@gmail.com>
2024-04-15 22:17:44 +0200

    Nueva ruta, capturando el body

---
+++ src/routes/mirror_body_string.rs
@@ -0,0 +1,4 @@
+pub async fn mirror_body_string(body: String) -> String {
+    // si queremos extraer solamente el body como cadena
+    body
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,14 @@
 mod hello_word;
+mod mirror_body_string;
 
-use axum::{body::Body, routing::get, Router};
+
+use axum::{body::Body, routing::{get, post}, Router};
 
 use hello_word::hello_word;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router<Body> {
-    Router::new().route("/", get(hello_word))
+    Router::new()
+        .route("/", get(hello_word))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
