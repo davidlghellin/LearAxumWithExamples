c923ab73c5376085b873117a3fdbf1c692c1b06d
David López <davidlghellin@gmail.com>
2024-04-15 20:28:08 +0200

    Refactor para uso de función async

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
 
-    let app: Router = Router::new().route("/", get(||async   {"Hola mundo"}      ));
+    let app: Router = Router::new().route("/", get(hola_mundo));
     
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
     
+}
+
+async fn hola_mundo() -> String{
+    "Hola mundo".to_owned()
 }
