c12684babdfab344042fa72479b4659d73987c9b
David López <davidlghellin@gmail.com>
2024-04-16 22:01:30 +0200

    Quitamos los warnings

--- src/routes/validate_data_with_serde.rs
+++ src/routes/validate_data_with_serde.rs
@@ -3,15 +3,17 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct RequestUser {
-    user: String,
-    pass: String,
-    otro: Option<String>,
+    pub user: String,
+    pub pass: String,
+    pub otro: Option<String>,
 }
 
 pub async fn validate_data_with_serde(Json(user): Json<RequestUser>) {
     dbg!(user);
-  /*  Json(RequestUser{
+    /*  
+    Json(RequestUser{
         username:user.username,
         password:"Secre".to_owned(),
-    });*/
+    });
+    */
 }
