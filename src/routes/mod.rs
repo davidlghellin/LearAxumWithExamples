5e58d5a58bf531f508c5fbbb7be92b47d60b5642
David López <davidlghellin@gmail.com>
2024-04-16 08:31:06 +0200

    Middlewawre cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -6,7 +6,8 @@ mod query_params;
 mod mirror_user_agent;
 mod mirror_custom_agent;
 
-use axum::{body::Body, routing::{get, post}, Router};
+use axum::{body::Body, http::Method, routing::{get, post}, Router};
+use tower_http::cors::{Any, CorsLayer};
 
 use hello_word::hello_word;
 use mirror_body_string::mirror_body_string;
@@ -17,6 +18,12 @@ use mirror_user_agent::mirror_user_agent;
 use mirror_custom_agent::mirror_custom_agent;
 
 pub fn create_routes() -> Router<Body> {
+    let cors = CorsLayer::new()
+    // allow `GET` and `POST` when accessing the resource
+    .allow_methods([Method::GET, Method::POST])
+    // allow requests from any origin
+    .allow_origin(Any);
+
     Router::new()
         .route("/", get(hello_word))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -27,4 +34,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
         .route("/mirror_custom_agent", get(mirror_custom_agent))
+        .layer(cors)
 }
