257889dde2b97a023b3e33493922f32ee4f19953
David López <davidlghellin@gmail.com>
2024-04-25 18:28:28 +0200

    gitignore y no test async

--- src/routes/hello_word.rs
+++ src/routes/hello_word.rs
@@ -1,3 +1,9 @@
 pub async fn hello_word() -> String {
     "Hola mundo desde mi fichero".to_owned()
 }
+
+#[tokio::test]
+async fn test_something_async() {
+    let hola = hello_word().await;
+    assert_eq!(hola, "Hola mundo desde mi fichero".to_string())
+}
