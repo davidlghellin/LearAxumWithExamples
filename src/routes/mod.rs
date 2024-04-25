21b50b107a8c2d02faca2108c08a9a22489ae768
David López <davidlghellin@gmail.com>
2024-04-25 20:35:18 +0200

    test end point

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,37 +1,43 @@
+mod always_errors;
+mod custom_json_extractor;
+mod get_json;
 mod hello_word;
-mod mirror_body_string;
+mod middleware_message;
 mod mirror_body_json;
+mod mirror_body_string;
+mod mirror_custom_agent;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
-mod mirror_user_agent;
-mod mirror_custom_agent;
-mod middleware_message;
 mod read_middleware_custom_headers;
-mod set_middleware_custom;
-mod always_errors;
 mod returns_201;
-mod get_json;
+mod set_middleware_custom;
 mod validate_data_with_serde;
-mod custom_json_extractor;
 
-use axum::{body::Body, http::Method, middleware, routing::{get, post}, Extension, Router};
+use axum::{
+    body::Body,
+    http::Method,
+    middleware,
+    routing::{get, post},
+    Extension, Router,
+};
 use tower_http::cors::{Any, CorsLayer};
 
+use always_errors::always_errors;
+use custom_json_extractor::custom_json_extractor;
+use get_json::get_json;
 use hello_word::hello_word;
-use mirror_body_string::mirror_body_string;
+use middleware_message::middleware_message;
 use mirror_body_json::mirror_body_json;
+use mirror_body_string::mirror_body_string;
+use mirror_custom_agent::mirror_custom_agent;
+use mirror_user_agent::mirror_user_agent;
 use path_variables::{hard_coded_patch, path_variables};
 use query_params::query_params;
-use mirror_user_agent::mirror_user_agent;
-use mirror_custom_agent::mirror_custom_agent;
-use middleware_message::middleware_message;
 use read_middleware_custom_headers::read_middleware_custom_headers;
-use set_middleware_custom::set_middleware_custom;
-use always_errors::always_errors;
 use returns_201::returns_201;
-use get_json::get_json;
+use set_middleware_custom::set_middleware_custom;
 use validate_data_with_serde::validate_data_with_serde;
-use custom_json_extractor::custom_json_extractor;
 
 #[derive(Clone)]
 pub struct SharedDate {
@@ -40,15 +46,20 @@ pub struct SharedDate {
 
 pub fn create_routes() -> Router<Body> {
     let cors = CorsLayer::new()
-    // allow `GET` and `POST` when accessing the resource
-    .allow_methods([Method::GET, Method::POST])
-    // allow requests from any origin
-    .allow_origin(Any);
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any);
 
-    let shared_data=SharedDate{ message:"hola desde shared".to_owned()};
+    let shared_data = SharedDate {
+        message: "hola desde shared".to_owned(),
+    };
 
     Router::new()
-        .route("/read_middleware_custom_headers", get(read_middleware_custom_headers))
+        .route(
+            "/read_middleware_custom_headers",
+            get(read_middleware_custom_headers),
+        )
         .route_layer(middleware::from_fn(set_middleware_custom))
         .route("/", get(hello_word))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -67,5 +78,36 @@ pub fn create_routes() -> Router<Body> {
         .route("/get_json", get(get_json))
         .route("/validate_data_with_serde", post(validate_data_with_serde))
         .route("/custom_json_extractor", post(custom_json_extractor))
+}
 
+mod test {
+
+    #[tokio::test]
+    async fn test_endpoint_basic() {
+        use axum::http::StatusCode;
+        use axum::Router;
+        use axum_test_helper::TestClient;
+
+        // you can replace this Router with your own app
+        let app = Router::new().route("/", axum::routing::get(|| async {}));
+
+        // initiate the TestClient with the previous declared Router
+        let client = TestClient::new(app);
+        let res = client.get("/").send().await;
+        assert_eq!(res.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_endpoint_hello() {
+        use axum::Router;
+        use axum_test_helper::TestClient;
+
+        // you can replace this Router with your own app
+        let app = Router::new().route("/", axum::routing::get(crate::routes::hello_word));
+
+        // initiate the TestClient with the previous declared Router
+        let client = TestClient::new(app);
+        let res: axum_test_helper::TestResponse = client.get("/").send().await;
+        assert_eq!(res.text().await, "Hola mundo desde mi fichero".to_owned());
+    }
 }
